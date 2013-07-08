ba96da211ce98757608640e373925a91f0cf1c2e
Oliver Lloyd <oliver_lloyd@hotmail.com>
2013-07-08 16:40:41 +0100

    poist

--- server/routes/routes_status.js
+++ server/routes/routes_status.js
@@ -3,4 +3,7 @@ module.exports = function(app) {
   app.get('*', function(req, res){
     res.send(200, SIMEPL.status);
   });  
+  app.post('*', function(req, res){
+    res.send(200, SIMEPL.status);
+  });  
 };
